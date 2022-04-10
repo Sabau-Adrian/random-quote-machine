e175aa76abee3f5c41461cc199a2bacbc9525c4a
Sabau Adrian <sabau.adrian1995@gmail.com>
2022-04-10 20:25:56 +0300

    completed react project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import {FaTwitterSquare} from 'react-icons/fa'
+import colorsArray from './Colors.js'
+
+let quoteDataBase = "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+
 
 function App() {
+  const [quote, setQuote] = useState("The way to get started is to quit talking and begin doing.")
+  const [author, setAuthor] = useState("Walt Disney")
+  const [randomNumber, setRandomNumber] = useState("0")
+  const [quotes, setQuotes] = useState(null)
+  const [randomColor, setRandomColor] = useState('#CFD8DC')
+
+  const fetchQuotes = async (url) => {
+    const response = await fetch(url)
+    const parsedJSON = await  response.json()
+    setQuotes(parsedJSON.quotes)
+    console.log(parsedJSON)
+  }
+
+  useEffect(() => {
+    fetchQuotes(quoteDataBase)
+  }, [quoteDataBase])
+
+
+  
+
+  const changeQuoteAndAuthor = () => {
+    let randomInteger = Math.floor(Math.random() * quotes.length)
+     setRandomNumber(randomInteger)
+     setQuote(quotes[randomInteger].quote)
+     setAuthor(quotes[randomInteger].author)
+     setRandomColor(colorsArray[randomNumber])
+
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header" style = {{backgroundColor: randomColor}}>
+    <div id="quote-box" style = {{ color: randomColor}}>
+      <p id="text">"{quote}"</p>
+      <p id ="author">- {author}</p>
+      <a id="tweet-quote"
+      href={`http://www.twitter.com/intent/tweet?text=${quote} -${author}`}><FaTwitterSquare style={{ color: randomColor, height: "40px", width: "40px"}} /></a>
+      <button style={{backgroundColor: randomColor, color: "white"}}id="new-quote" onClick={() =>changeQuoteAndAuthor()}>Change Quote</button>
+    </div>
+    </header>
     </div>
   );
 }
